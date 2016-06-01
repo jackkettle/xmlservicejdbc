693f580563bea6aa7500465fe94da5efb270d014
Jack Kettle <jack.kettle@terminalfour.com>
2016-06-01 17:27:21 +0100

    Refactoring

---
+++ src/main/java/com/terminalfour/database/xmlservicejdbc/core/Protocol.java
@@ -0,0 +1,33 @@
+/*
+ * (C) 2016 TERMINALFOUR Solutions Ltd.
+ *
+ * Author: Paul Kelly
+ * Created: 30 May 2016
+ */
+package com.terminalfour.database.xmlservicejdbc.core;
+
+public enum Protocol {
+    HTTP("http://"),
+    HTTPS("https://"),
+    FILE("file:/"),
+    INVALID("");
+
+    private String initial;
+
+    Protocol (final String initial) {
+        this.initial = initial;
+    }
+
+    public static Protocol getProtocol (final String url) {
+        if (url == null)
+            return INVALID;
+
+        String lUrl = url.toLowerCase ();
+        for (Protocol next : values ()) {
+            if (lUrl.startsWith (next.initial))
+                return next;
+        }
+
+        return INVALID;
+    }
+}
